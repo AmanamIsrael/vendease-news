0fca60e38d87dbec88b2dea682570356a805658d
Amanam Israel <amanamisrael@gmail.com>
2022-11-13 09:42:36 +0100

    Feature: add dynamic title when changing pages

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from "next/router"
 import Tabs from "../components/Tabs"
 
 interface MainLayoutProps {
@@ -5,10 +6,11 @@ interface MainLayoutProps {
 }
 
 const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
+    const title = useRouter().pathname === '/' ? 'News feed' : 'Bookmarks'
     return (
         <>
             <section className='h-screen pt-[32px]'>
-                <p className='leading-44.78 text-4xl text-green mb-[32px] capitalise text-center'>News Feed</p>
+                <p className='leading-44.78 text-4xl text-green mb-[32px] capitalise text-center'>{title}</p>
                 <div className='grid gap-[32px] justify-items-center md:w-[600px] w-[350px] mx-auto'>
                     <Tabs />
                     <main className="w-full">
